8d748aadb13d75f98edb32001f6c0d53c5e89ea1
LouisDsr <92309267+LouisDsr@users.noreply.github.com>
2021-10-11 17:14:20 +0200

    Update Ninja.java

--- Ninja.java
+++ Ninja.java
@@ -1,15 +1,15 @@
 package naruto;
 
 public class Ninja extends Personnage{
-    protected element Myelement;
+    protected Element Myelement;
     protected int NbKunai;
     
-    public Ninja(String Nom, VillageCache village, int Puissance, boolean EnVie, element Myelement, int NbKunai){
-        super(Nom, Village, Puissance, EnVie);
+    public Ninja(String nom, VillageCache village, int puissance, boolean EnVie, Element Myelement, int NbKunai){
+        super(nom, village, puissance);
         this.Myelement = Myelement;
         this.NbKunai = 10;
-        SePresenter();
+        //SePresenter();
     }
-    public float getMyelement() { return this.Myelement; }
+    public Element getMyelement() { return this.Myelement; }
     public void Ravitaillement(int value) { this.NbKunai = value + this.NbKunai; }
 }
