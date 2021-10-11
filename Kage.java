3a1196f60fa588ac491ccf5c49eaf594a72ac00b
LouisDsr <92309267+LouisDsr@users.noreply.github.com>
2021-10-11 17:13:50 +0200

    Update Kage.java

--- Kage.java
+++ Kage.java
@@ -3,10 +3,10 @@ package naruto;
 public class Kage extends Ninja{
     private int NumeroKage;
     
-    public Kage(String Nom, VillageCache village, int Puissance, boolean EnVie, element Myelement, int NbKunai, int NumeroKage, ){
-        super(Nom, Village, Puissance, EnVie, Myelement, NbKunai);
+    public Kage(String nom, VillageCache village, int puissance, boolean EnVie, Element Myelement, int NbKunai, int NumeroKage){
+        super(nom, village, puissance, EnVie, Myelement, NbKunai);
         this.NumeroKage = NumeroKage;
-        SePresenter();
+        //SePresenter();
     }
     public int getNumeroKage() { return this.NumeroKage; }
 }
